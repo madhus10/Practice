4a9c16365265909e984bbd2b7067726e990bedcd
Madhu Shivarudrappa <Madhu.Shivarudrappa@in.ey.com>
2019-08-21 23:20:24 +0530

    my latest commit

---
+++ src/test/java/AutomationDemo/Practice/SearchPage.java
@@ -0,0 +1,16 @@
+package AutomationDemo.Practice;
+
+import org.testng.annotations.Test;
+
+public class SearchPage {
+
+	
+		// TODO Auto-generated method stub
+		@Test
+		public void test() {
+			System.out.println("Test printed");
+		}
+
+	}
+
+
