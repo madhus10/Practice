d694e77b846d7f45177388ae32f6ced9037f0f8b
Madhu Shivarudrappa <Madhu.Shivarudrappa@in.ey.com>
2019-08-21 17:36:08 +0530

    my first git commit

---
+++ src/test/java/AutomationDemo/Practice/AppTest.java
@@ -0,0 +1,15 @@
+package AutomationDemo.Practice;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+
+{
+	@Test
+	public void test1() {
+		System.out.println("Test method");
+	}
+}
