5c73628b39e5f3985edabd38a62c2ad4452870bb
Madhu Shivarudrappa <Madhu.Shivarudrappa@in.ey.com>
2019-08-21 23:49:46 +0530

    my latest commit test2

--- src/test/java/AutomationDemo/Practice/SearchPage.java
+++ src/test/java/AutomationDemo/Practice/SearchPage.java
@@ -4,17 +4,19 @@ import org.testng.annotations.Test;
 
 public class SearchPage {
 
-	
-		// TODO Auto-generated method stub
-		@Test
-		public void test() {
-			System.out.println("Test printed");
-		}
+	// TODO Auto-generated method stub
 	@Test
-		public void test1() {
-			System.out.println("Test1 printed");
-		}
-
+	public void test() {
+		System.out.println("Test printed");
 	}
 
+	@Test
+	public void test1() {
+		System.out.println("Test1 printed");
+	}
+	@Test
+	public void test2() {
+		System.out.println("Test2 printed");
+	}
 
+}
